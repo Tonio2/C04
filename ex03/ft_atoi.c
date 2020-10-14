5cb7dfd68eab5f0b5a551392f608dd5dfe40a9bf
Antoine Labalette <alabalet@student.42nice.fr>
2020-10-14 23:45:35 +0200

    [a] ex03

--- ex03/ft_atoi.c
+++ ex03/ft_atoi.c
@@ -6,8 +6,28 @@
 /*   By: alabalet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/14 23:08:57 by alabalet          #+#    #+#             */
-/*   Updated: 2020/10/14 23:09:10 by alabalet         ###   ########.fr       */
+/*   Updated: 2020/10/14 23:45:01 by alabalet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_atoi(char *str)
+{
+	int cpt;
+	int sign;
+	int nb;
 
+	cpt = 0;
+	sign = 1;
+	nb = 0;
+	while (str[cpt] == ' ' || (9 <= str[cpt] && str[cpt] <= 13))
+		cpt++;
+	while (str[cpt] == '-' || str[cpt] == '+')
+	{
+		if (str[cpt] == '-')
+			sign *= -1;
+		cpt++;
+	}
+	while ('0' <= str[cpt] && str[cpt] <= '9')
+		nb = nb * 10 + str[cpt++] - '0';
+	return (sign * nb);
+}
